3e01d152f0ba05704995fb31a158819fc0fd3d1c
Vishnu Sahani <51869991+VishnuSahani@users.noreply.github.com>
2026-01-28 16:53:49 +0530

    Initialize Express server with routes and middleware
    Set up Express server with CORS and error handling.

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 
+const express = require("express"); //1
+const cors = require("cors"); //2
+const openAIrouter = require("./router/openAI.route");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: false,
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/bot", openAIrouter);
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, error: "Hello" });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  console.error(err);
+  res
+    .status(500)
+    .json({ success: false, error: "Something went wrong!", message: err });
+});
+
+// 404 handler (should be placed at the end of all routes)
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.originalUrl,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
